40c1a6338b4df8b2d42e3aaf65deef20a0fb700d
bruizpalen <115789234+bruizpalen@users.noreply.github.com>
2023-09-19 21:29:43 +0000

    First version

--- src/models.py
+++ src/models.py
@@ -1,49 +1,60 @@
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class User(Base):
+class Users(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False)
     firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+
+    comments = relationship('Comments', backref='user', lazy=True)
+    posts = relationship('Posts', backref='user', lazy=True)
+
+    followers = relationship('Followers', foreign_keys='Followers.following_id', backref='following', lazy=True)
+    following = relationship('Followers', foreign_keys='Followers.follower_id', backref='followers', lazy=True) 
     
-class Post(Base):
-    __tablename__ = 'post'
+class Posts(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
+
+    comments = relationship('Comments', backref='post', lazy=True)
+    media = relationship('Media', secondary='PostMediaConnectio', lazy='subquery', backref= 'posts')
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
     url = Column(String(250), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'))
 
-class Follower(Base):
+class PostMediaConnection(Base):
+    __tablename__ = 'post_media_connection'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    media_id = Column(Integer, ForeignKey('media.id'))
+
+
+
+class Followers(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    following_id = Column(Integer, ForeignKey('user.id'))
     
-class Comment(Base):
+class Comments(Base):
     __tablename__ = 'comment'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(250), nullable=False)
-    author_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
-    # person = relationship(Person)
+
 
     def to_dict(self):
         return {}
